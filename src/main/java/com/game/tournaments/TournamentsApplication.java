46482a820d0c786ed0fd424fb5970f0057287260
mario <mariocapri01@gmail.com>
2026-02-05 11:39:34 +0100

    Hola Mundo

--- src/main/java/com/game/tournaments/TournamentsApplication.java
+++ src/main/java/com/game/tournaments/TournamentsApplication.java
@@ -8,8 +8,7 @@ public class TournamentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TournamentsApplication.class, args);
-		//probando probando uno dos
-		//probando rama develop
+		System.out.println("Hola Mundo");
 	}
 
 }
