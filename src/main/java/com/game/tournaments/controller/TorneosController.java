9f14e3ea726266025a6aa1eee5d624ce5fbb7de6
mario <mariocapri01@gmail.com>
2026-02-23 14:28:43 +0100

    Conexion BBDD

---
+++ src/main/java/com/game/tournaments/controller/TorneosController.java
@@ -0,0 +1,10 @@
+package com.game.tournaments.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/torneos")
+public class TorneosController {
+
+}
