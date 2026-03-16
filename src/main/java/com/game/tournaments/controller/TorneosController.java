d7a2ca36a6ca0244cd39470650de4cf934687761
mario <mariocapri01@gmail.com>
2026-03-16 12:31:21 +0100

    Hola Mundo en Postman

--- src/main/java/com/game/tournaments/controller/TorneosController.java
+++ src/main/java/com/game/tournaments/controller/TorneosController.java
@@ -2,9 +2,16 @@ package com.game.tournaments.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
-@RequestMapping("/torneos")
+@RequestMapping("/api/torneos")
 public class TorneosController {
-
+    @GetMapping("/holamundo")
+    public String getHolaMundo() {
+        return "Hola Mundo";
+    }
+    
 }
