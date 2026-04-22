5a9f752f2adcb53abace7a8ba9aba43f2af0e424
mario <mariocapri01@gmail.com>
2026-04-22 09:56:42 +0200

    Optimiza Emparejamiento

--- src/main/java/com/game/tournaments/model/Emparejamiento.java
+++ src/main/java/com/game/tournaments/model/Emparejamiento.java
@@ -5,16 +5,11 @@ import java.util.ArrayList;
 
 public class Emparejamiento {
     private List<String> jugadores;
-    private int maxNumJugadores;
     public Emparejamiento(){
-        this.maxNumJugadores = 2;
         this.jugadores = new ArrayList<>();
     }
     public boolean nuevoJugador(String jugador){
-        if(jugadores.size() < maxNumJugadores) {
-            return this.jugadores.add(jugador);
-        }
-        return false;
+        return this.jugadores.add(jugador);
     }
     public void setJugadores(List<String> jugadores){
         this.jugadores = jugadores;
